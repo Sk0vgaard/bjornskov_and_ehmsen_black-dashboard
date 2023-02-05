613f4b5fd159889850083014ef9e1451dbfe2447
Mathias Skovgaard <sk0vgaard@live.dk>
2023-02-05 10:33:49 +0100

    - Add carpenter img to frontpage and fixed some scss

--- src/app/pages/front-page/front-page.component.ts
+++ src/app/pages/front-page/front-page.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-front-page',
   templateUrl: './front-page.component.html',
+  styleUrls: ['./front-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FrontPageComponent {}
