724f2c0c72acd594207a0cc475bdda773b22963e
Skovgaard <sk0vgaard@live.dk>
2023-07-03 20:54:14 +0200

    Refactor front-page with new images and setup

--- src/app/pages/front-page/front-page.component.ts
+++ src/app/pages/front-page/front-page.component.ts
@@ -9,13 +9,34 @@ import { PrimaryWorkingAreaModel } from '../../_models/primary-working-area.mode
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FrontPageComponent {
-  public carpenterModel: PrimaryWorkingAreaModel = {
+  public introModel: PrimaryWorkingAreaModel = {
     cardModel: {
-      title: 'Tømrer / Snedkerarbejde',
+      title: 'Tømrer / Snedker / Maskinsnedker / Betonarbejder',
       description:
-        'Hos Bjørnskov & Ehmsen råder vi over tømre, snedkere & maskinsnedkere. Vi dækker et bredt spektrum af træarbejde, lige fra tømrearbejde med opstilling af tagkonstruktioner til fine snedkerarbejde, som fremstilling af viduer og døre. Vores abition er at sikre topresultater af højeste kvalitet. Uanset omfanget af opgaven, er vores engagement i at udføre vores arbejde med et skarpt fokus på kommunikation, præcision og nøjagtighed, så du får den bedst mulige oplevelse, samt kan være med hele vejen igennem.',
+        'Uanset om dine tømrebehov, store som små, privat eller erhverv, står vi hos Bjørnskov & Ehmsen klar til at imødekomme dine forventninger. Vi gør din vision til virkelighed, fra idé til fuldendt projekt, og har altid stolthed i at levere vores arbejde præcist og med højeste kvalitetsstandard. Vi har vores hjemsted i Esbjerg Ø, men vores rækkevidde er landsdækkende - vores løsninger og produkter kan findes i hele Danmark. Med en betydelig portefølje af succesfulde projekter i Vestjylland, står vi stærkt i vores engagement og ekspertise.' +
+        '<br>\n' +
+        '<br>\n' +
+        'Hvis du vælger os som din samarbejdspartner i byggeprojektet, vil du møde et byggefirma, som påtager sig\n' +
+        'ansvaret for alle byggeprocesserne, alt imens vi prioriterer at kundens behov er i fokus.\n' +
+        '<br>\n' +
+        '<br>\n' +
+        '<b><u>Hos Bjørnskov & Ehmsen handler det om mere end blot at levere et resultat.</u></b>\n' +
+        'Vi stræber efter at skabe værdi for vores kunder. Hver opgave behandles med personlig opmærksomhed og\n' +
+        'omhu, som sikrer at det færdige arbejde opfylder dine forventninger. Med vores brede portfolio af\n' +
+        'tjenester, kan vi håndtere alle aspekter af dit tømrerprojekt, hvilket betyder at du kan slappe af og lade\n' +
+        'os om det hårde arbejde. Så, uanset behov, så lad os hjælpe dig med at gøre din drøm til virkelighed.',
     },
     imageUrl: 'assets/be_icons/be_carpenter.png',
+    imageAlt: 'Tømrer/Snedkerarbejde image',
+  };
+
+  public carpenterModel: PrimaryWorkingAreaModel = {
+    cardModel: {
+      title: 'Alle Opgaver',
+      description:
+        'Hos Bjørnskov & Ehmsen råder vi over tømre, snedkere, maskinsnedkere & betonarbejder(element montage). Vi dækker et bredt spektrum af træarbejde, lige fra tømrearbejde med opstilling af tagkonstruktioner til fine snedkerarbejde, såsom fremstilling af viduer og døre. Vores abition er at sikre topresultater af højeste kvalitet. Uanset omfanget af opgaven, er vores engagement i at udføre vores arbejde med et skarpt fokus på kommunikation, præcision og nøjagtighed, så du får den bedst mulige oplevelse, samt kan være med hele vejen igennem.',
+    },
+    imageUrl: 'assets/be_images/be_all-tasks.jpg',
     imageAlt: 'Tømrer/Snedkerarbejde image',
   };
 
@@ -23,7 +44,7 @@ export class FrontPageComponent {
     cardModel: {
       title: 'Hoved- og Totalentreprise',
       description:
-        'Bjørnskov & Ehmsen tilbyder to hovedtyper af entrepriser: hovedentreprise og totalentreprise. I en hovedentreprise overtager vi ansvaret for det meste af arbejdet, mens bygherren stiller tegnings- og beskrivelsesmateriale til rådighed. I en totalentreprise håndterer vi hele processen fra planlægning til endelig færdiggørelse. Uanset hvilken entrepriseform du vælger, garanterer vi tydelige aftaler vedrørende arbejdets omfang, tidslinje og budget for at sikre en professionel og pålidelig byggeproces.',
+        'Bjørnskov & Ehmsen tilbyder to hovedtyper af entrepriser: <u><b>hovedentreprise</b></u> og <u><b>totalentreprise</b></u>. I en hovedentreprise overtager vi ansvaret for det meste af arbejdet, mens bygherren stiller tegnings- og beskrivelsesmateriale til rådighed. I en totalentreprise håndterer vi hele processen fra planlægning til endelig færdiggørelse. Uanset hvilken entrepriseform du vælger, garanterer vi tydelige aftaler vedrørende arbejdets omfang, tidslinje og budget for at sikre en professionel og pålidelig byggeproces.',
     },
     imageUrl: 'assets/be_images/be_enterprise.jpg',
     imageAlt: 'Hoved- og totalentreprise image',
