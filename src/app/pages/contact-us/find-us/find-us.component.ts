47c08f7cb1fd6f7f45b40d533d68b184d4d9addb
Skovgaard <sk0vgaard@live.dk>
2023-07-03 23:46:53 +0200

    Add more correct google map direction

--- src/app/pages/contact-us/find-us/find-us.component.ts
+++ src/app/pages/contact-us/find-us/find-us.component.ts
@@ -7,17 +7,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class FindUsComponent {
   address = 'Hededammen 9, 6705 Esbjerg Ø';
+  googleMapUrl = 'Bjørnskov & Ehmsen tømrer og snedker, Hededammen 9, 6705 Esbjerg, Denmark';
   facebookUrl = 'https://www.facebook.com/profile.php?id=100089879839188';
   instagramUrl = 'https://www.instagram.com/';
   linkedinUrl = 'https://www.linkedin.com/feed/';
 
   public navigateToAddress(): void {
-    window.open(`https://www.google.com/maps/search/${encodeURI(this.address)}`);
+    window.open(`https://www.google.com/maps/search/${encodeURI(this.googleMapUrl)}`);
   }
 
   public openFacebook(): void {
     window.open(this.facebookUrl, '_blank');
   }
+
+  // https://xn--bjrnskov-ehmsen-6tb.dk/
+  // http://www.xn--bjrnskov-ehmsen-6tb.dk/
 
   public openInstagram(): void {
     window.open(this.instagramUrl, '_blank');
