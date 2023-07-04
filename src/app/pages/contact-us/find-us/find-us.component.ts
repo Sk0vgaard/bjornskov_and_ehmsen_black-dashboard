03bd849ea94acd3482b70438914c897a7a4353ae
Skovgaard <sk0vgaard@live.dk>
2023-07-04 17:43:47 +0200

    Make text smaller in googleMaps btn

--- src/app/pages/contact-us/find-us/find-us.component.ts
+++ src/app/pages/contact-us/find-us/find-us.component.ts
@@ -6,7 +6,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FindUsComponent {
-  address = 'Hededammen 9, 6705 Esbjerg Ø';
+  address = 'Hededammen 9, Esbjerg Ø';
   googleMapUrl = 'Bjørnskov & Ehmsen tømrer og snedker, Hededammen 9, 6705 Esbjerg, Denmark';
   facebookUrl = 'https://www.facebook.com/profile.php?id=100089879839188';
   instagramUrl = 'https://www.instagram.com/';
@@ -19,9 +19,6 @@ export class FindUsComponent {
   public openFacebook(): void {
     window.open(this.facebookUrl, '_blank');
   }
-
-  // https://xn--bjrnskov-ehmsen-6tb.dk/
-  // http://www.xn--bjrnskov-ehmsen-6tb.dk/
 
   public openInstagram(): void {
     window.open(this.instagramUrl, '_blank');
