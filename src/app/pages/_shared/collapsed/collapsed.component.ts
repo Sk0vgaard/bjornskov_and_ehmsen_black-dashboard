5ad6089d1946888f72314d1fd789e364c810de6a
Skovgaard <sk0vgaard@live.dk>
2023-08-28 18:57:31 +0200

    - Add input for if collapsed.component should be collapsed as default or not

--- src/app/pages/_shared/collapsed/collapsed.component.ts
+++ src/app/pages/_shared/collapsed/collapsed.component.ts
@@ -12,9 +12,10 @@ import { CardInfoModel } from '../../../_models/card-info.model';
 })
 export class CollapsedComponent {
   @Input() cardInfoModel: CardInfoModel;
+  @Input() collapseAsDefault: boolean = true;
+
+  public isCollapsed: boolean;
 
   faChevronUp = faChevronUp;
   faChevronDown = faChevronDown;
-
-  public isCollapsed: boolean;
 }
