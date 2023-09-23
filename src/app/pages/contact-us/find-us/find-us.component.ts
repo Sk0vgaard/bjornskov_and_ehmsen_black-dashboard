45f6581c14d7a9f378423ff6a43266e109df08a2
Skovgaard <sk0vgaard@live.dk>
2023-09-23 12:03:59 +0200

    Add company linkedin

--- src/app/pages/contact-us/find-us/find-us.component.ts
+++ src/app/pages/contact-us/find-us/find-us.component.ts
@@ -11,7 +11,7 @@ export class FindUsComponent {
   googleMapUrl = 'Bjørnskov & Ehmsen tømrer og snedker, Hededammen 9, 6705 Esbjerg, Denmark';
   facebookUrl = 'https://www.facebook.com/profile.php?id=100089879839188';
   instagramUrl = 'https://www.instagram.com/';
-  linkedinUrl = 'https://www.linkedin.com/feed/';
+  linkedinUrl = 'https://www.linkedin.com/company/bjornskov-ehmsen';
 
   public navigateToAddress(): void {
     window.open(`https://www.google.com/maps/search/${encodeURI(this.googleMapUrl)}`);
